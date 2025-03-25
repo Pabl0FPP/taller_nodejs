b4e090befa4dab2381f629b3f2580fe830a75313
RafaelaRuiz <iam1tonystark@gmail.com>
2025-03-24 19:19:00 -0500

    update indexes routes and services by candles

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,3 +2,4 @@ export * from './auth.route';
 export * from './user.route';
 export * from './fragance.route';
 export * from './container.route';
+export * from './candle.route';
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,4 @@ export * from './user.service';
 export * from './auth.service';
 export * from './container.service';
 export * from './fragance.service';
+export * from './candle.service';

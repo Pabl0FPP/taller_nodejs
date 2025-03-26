e628e716251a37f58c55a4be7a89e9521ab78143
Pablo Pineda <pinedapablo6718@gmail.com>
2025-03-25 22:49:34 -0500

    try fix deploy x2

--- dist/src/index.js
+++ dist/src/index.js
@@ -3,16 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const routes_1 = require("./routes");
 const connectionDB_1 = require("./lib/connectionDB");
-Object.defineProperty(exports, "db", { enumerable: true, get: function () { return connectionDB_1.db; } });
 const initialSetup_1 = require("./lib/initialSetup");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-exports.app = app;
 const port = process.env.PORT || 3000;
 (0, initialSetup_1.createRoles)();
 app.use(express_1.default.json());
@@ -25,3 +22,4 @@ app.use('/candle', routes_1.candleRouter);
 connectionDB_1.db.then(() => app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 }));
+exports.default = app;

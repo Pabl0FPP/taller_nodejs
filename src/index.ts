5e6fb39eea91ec4fe6131ad517e7bb227be49411
Pablo Pineda <pinedapablo6718@gmail.com>
2025-03-25 21:54:30 -0500

    fix vercel configuration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
-import { db, app } from "./app";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { userRouter, authRouter} from './routes';
+import { db } from "./lib/connectionDB";
+import { createRoles } from './lib/initialSetup';
 
+dotenv.config();
+
+const app: Express = express();
 const port: number = process.env.PORT as any || 3000;
+createRoles();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/user', userRouter);
+app.use('/auth', authRouter);
+
 
 db.then( () =>
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     } )
 );
+
+
+export { app, db };

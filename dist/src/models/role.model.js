a4772a367cb27c4ced49d62c824eab64753bb432
Pablo Pineda <pinedapablo6718@gmail.com>
2025-06-05 23:22:09 -0500

    por fin?

--- dist/src/models/role.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RoleModel = void 0;
-const mongoose_1 = require("mongoose");
-const roleSchema = new mongoose_1.Schema({
-    name: String
-});
-exports.RoleModel = (0, mongoose_1.model)("Role", roleSchema);
